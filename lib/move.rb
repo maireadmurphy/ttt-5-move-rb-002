91c21ed1290e7897152bf32b701556ae2af1ca0b
maireadmurphy <mairead.aw.murphy@gmail.com>
2016-01-17 09:19:40 +0000

    nitrous-backup at Sun Jan 17 09:19:40 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +9,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+def move(board, position, value = "X")
+end
